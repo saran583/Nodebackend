9bc1c18a49b535778e8665c02a5d6adab2cd032c
coder <contact@hariomtech.com>
2022-07-15 01:10:44 +0530

    Adding the basic routes

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,19 @@
 const http = require("http")
 
 const server = http.createServer((req,res)=>{
-res.write("Welcome to the nodejs Learnings");
-res.end();
+
+    if(req.url === "/about"){
+        res.end("Welcome to the About page")
+    }
+    else if(req.url === "/" || req.url === "/home"){
+        res.end("Welcome to the Homepage")
+    }
+    else{
+        res.end("<h1>Oops! wrong page that does not exist</h1>  <p>Check out our homepage <a href='/home'>Home</a></p> ")
+    }
+
 });
 
 server.listen(5000,()=>{
-    console.log("the server is runnign")
+    console.log("the server is running")
 });
