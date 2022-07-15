143ed6654599508a36e938178a9ac950d663d689
coder <contact@hariomtech.com>
2022-07-16 00:57:56 +0530

    reading a file

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,5 @@
 const http = require("http")
-
+const {readFile} = require("fs");
 const server = http.createServer((req,res)=>{
 
     if(req.url === "/about"){
@@ -7,6 +7,15 @@ const server = http.createServer((req,res)=>{
     }
     else if(req.url === "/" || req.url === "/home"){
         res.end("Welcome to the Homepage")
+    }
+    else if(req.url === "/readfile"){
+        readFile("../nodejs&expressnotes.txt",'utf8',(err, result)=>{
+            if(err){
+                res.end("there is a error reading the file",err);
+            }
+            console.log("this is the file data",result);
+            res.end(result)
+        })
     }
     else{
         res.end("<h1>Oops! wrong page that does not exist</h1>  <p>Check out our homepage <a href='/home'>Home</a></p> ")
