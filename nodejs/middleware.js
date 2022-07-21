c12e5fe92081aa389b451cce7e05691b59b6c462
coder <contact@hariomtech.com>
2022-07-22 01:07:42 +0530

    adding multiple Loggers

--- nodejs/middleware.js
+++ nodejs/middleware.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const logger = require("./logger");
+const authorize = require("./authorize");
 
 // Instead of passing the logger to all the api calls we can directly pass it to the app.use which gives the exact same behaviour
 // app.use should on top of all the api calls to execute first
-app.use(logger);
+// when using multiple logger it executes in the given order
+app.use([authorize,logger]);
 
 // We can add a logger by the adding the function which accepts the req, res , next as parameters.
 // next - when using the midleware like logger we need next to send the execution from the middleware to the api method
