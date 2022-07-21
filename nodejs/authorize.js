950df539189e8c2617978f20e20ba98d185fe21f
coder <contact@hariomtech.com>
2022-07-22 01:15:17 +0530

    Adding conditions to the middleware

--- nodejs/authorize.js
+++ nodejs/authorize.js
@@ -1,6 +1,12 @@
 const authorize =(req,res,next)=>{
-    console.log("Authorize")
-    next();
+    const {user} = req.query;
+    if(user === "john"){
+        req.user = {name:"john",id:5}
+        next();
+    }
+    else{
+        res.status(401).send("The user id not recognized by the Databse");
+    }
 }
 
 module.exports = authorize;
