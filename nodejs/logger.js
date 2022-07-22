6eb20635347a4df739ac72ca2e198767fafe1ef9
coder <contact@hariomtech.com>
2022-07-23 00:09:28 +0530

    added delete method

--- nodejs/logger.js
+++ nodejs/logger.js
@@ -1,6 +1,6 @@
 const logger =(req, res, next)=>{
-    const {method, url } = req;
-    console.log({method: method,url:url});
+    const {method, url, params } = req;
+    console.log({method: method,url:url,params:params});
     // if next is not declared then the browser keeps on loading since the execution is not passed to the intended function
     next();
 }
--- nodejs/middleware.js
+++ nodejs/middleware.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const logger = require("./logger");
 const authorize = require("./authorize");
+const data = require("./data.json")
 
 // Using  a third party middleware
 const morgan = require("morgan");
@@ -12,12 +13,29 @@ const morgan = require("morgan");
 
 // app.use([authorize,logger]);
 app.use(morgan('tiny'))
+// app.use(logger);
 
 // We can add a logger by the adding the function which accepts the req, res , next as parameters.
 // next - when using the midleware like logger we need next to send the execution from the middleware to the api method
 app.get("/",(req,res)=>{
     console.log("this is the get method");
     res.send("done check your console");
+})
+
+app.delete("/removehero/:name",(req,res)=>{
+    // const {name} = req.params;
+    console.log(req.params.name)
+    console.log(data)
+    var newherolist = data.filter((heros)=>heros.Name !== req.params.name);
+    console.log(newherolist);
+
+    if(!newherolist){
+        res.status(400).json({success:false,msg:"No heros exist on the given Name"})
+    }
+    else{
+        res.status(200).json({success:true,msg:"Hero deleted successfully",data:newherolist});
+    }
+
 })
 
 app.listen(5000,()=>{
