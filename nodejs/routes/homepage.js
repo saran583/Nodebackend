fe2c5f510f1883a281c3615fe917aa5d6e5c4fe1
coder <contact@hariomtech.com>
2022-07-25 00:23:58 +0530

    Alternate way of using routes

--- nodejs/routes/homepage.js
+++ nodejs/routes/homepage.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const {homepage,homepagepost,homepage2,homepage3} = require("./homepagecontroller.js");
 
 
-router.get("/homepage",homepage);
+// router.get("/homepage",homepage);
 
-router.post("/homepage",homepagepost);
+// router.post("/homepage",homepagepost);
 
-router.get("/homepage2",homepage2);
+// router.get("/homepage2",homepage2);
 
-router.get("/homepage3",homepage3);
+// router.get("/homepage3",homepage3);
+
+
+// Routers can also be called like this
+router.route('/homepage').get(homepage).post(homepagepost);
+router.route("/homepage2").get(homepage2);
+router.route("/homepage3").get(homepage3);
 
 
 module.exports = router;
